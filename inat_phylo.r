0f3f6057a63d47c1ed20b30459e07dac2f77125e
Lauren Chan <lchan@pacificu.edu>
2019-07-23 09:17:41 -0700

    fix

--- inat_phylo.r
+++ inat_phylo.r
@@ -6,7 +6,8 @@ proj_obs <- get_inat_obs_project(proj_dat$id, type="observations")
 
 spp_list <- unique(proj_obs$Scientific.name)
 
-#dltree <- datelife_search(input=spp_list)
+
+dltree <- datelife_search(input=spp_list, summary_format="phylo_median")
 pttree <- taxa_get_otol_tree(spp_list)
 
 #plot(dltree)
