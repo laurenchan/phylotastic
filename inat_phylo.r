26c5a30848dd82ddea5570e607b99144e8fc89f8
Lauren Chan <lchan@pacificu.edu>
2019-07-22 21:16:42 -0700

    Create inat_phylo.r

---
+++ inat_phylo.r
@@ -0,0 +1,15 @@
+library(rinat)
+library(rphylotastic)
+
+proj_dat <- get_inat_obs_project("pu-vert-zoo", type="info", raw=FALSE)
+proj_obs <- get_inat_obs_project(proj_dat$id, type="observations")
+
+spp_list <- unique(proj_obs$Scientific.name)
+
+#dltree <- datelife_search(input=spp_list)
+pttree <- taxa_get_otol_tree(spp_list)
+
+#plot(dltree)
+
+plot(pttree)
+
